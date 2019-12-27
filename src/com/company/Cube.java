e95ccabe88529bdf16d4e21f1c8f2953977de22e
arshyajabbi <arshyajabbari@gmail.com>
2019-12-27 23:43:32 +0330

    inverse turn finished

--- src/com/company/Cube.java
+++ src/com/company/Cube.java
@@ -124,5 +124,127 @@ public class Cube {
         }
     }
 
+    public void invTurn(int side){
+        int tmp=this.colors[side-1][2];
+        int tmp1=0;
+        int tmp2=0;
+
+        this.colors[side-1][2]=this.colors[side-1][0];
+        this.colors[side-1][0]=tmp;
+        this.colors[side-1][0]=this.colors[side-1][1];
+        this.colors[side-1][1]=tmp;
+        this.colors[side-1][1]=this.colors[side-1][3];
+        this.colors[side-1][3]=tmp;
+
+        if (side == 1){
+            tmp1=this.colors[5][2];
+            tmp2=this.colors[5][3];
+            this.colors[5][2]=this.colors[3][1];
+            this.colors[5][3]=this.colors[3][0];
+            this.colors[3][1]=tmp1;
+            this.colors[3][0]=tmp2;
+
+            this.colors[3][1]=this.colors[2][1];
+            this.colors[3][0]=this.colors[2][0];
+            this.colors[2][1]=tmp1;
+            this.colors[2][0]=tmp2;
+
+            this.colors[2][1]=this.colors[1][1];
+            this.colors[2][0]=this.colors[1][0];
+            this.colors[1][1]=tmp1;
+            this.colors[1][0]=tmp2;
+        }
+        if (side == 2){
+            tmp1=this.colors[1][0];
+            tmp2=this.colors[1][2];
+            this.colors[1][0]=this.colors[2][0];
+            this.colors[1][2]=this.colors[2][2];
+            this.colors[2][0]=tmp1;
+            this.colors[2][2]=tmp2;
+
+            this.colors[2][0]=this.colors[4][0];
+            this.colors[2][2]=this.colors[4][2];
+            this.colors[4][0]=tmp1;
+            this.colors[4][2]=tmp2;
+
+            this.colors[4][0]=this.colors[5][0];
+            this.colors[4][2]=this.colors[5][2];
+            this.colors[5][0]=tmp1;
+            this.colors[5][2]=tmp2;
+        }
+        if (side == 3){
+            tmp1=this.colors[0][2];
+            tmp2=this.colors[0][3];
+            this.colors[0][2]=this.colors[3][0];
+            this.colors[0][3]=this.colors[3][2];
+            this.colors[3][0]=tmp1;
+            this.colors[3][2]=tmp2;
+
+            this.colors[3][0]=this.colors[4][1];
+            this.colors[3][2]=this.colors[4][0];
+            this.colors[4][1]=tmp1;
+            this.colors[4][0]=tmp2;
+
+            this.colors[4][1]=this.colors[1][3];
+            this.colors[4][0]=this.colors[1][1];
+            this.colors[1][3]=tmp1;
+            this.colors[1][1]=tmp2;
+        }
+        if (side == 4){
+            tmp1=this.colors[5][1];
+            tmp2=this.colors[5][3];
+            this.colors[5][1]=this.colors[4][1];
+            this.colors[5][3]=this.colors[4][3];
+            this.colors[4][1]=tmp1;
+            this.colors[4][3]=tmp2;
+
+            this.colors[4][1]=this.colors[2][1];
+            this.colors[4][3]=this.colors[2][3];
+            this.colors[2][1]=tmp1;
+            this.colors[2][3]=tmp2;
+
+            this.colors[2][1]=this.colors[0][1];
+            this.colors[2][3]=this.colors[0][3];
+            this.colors[0][1]=tmp1;
+            this.colors[0][3]=tmp2;
+        }
+        if (side == 5){
+            tmp1=this.colors[5][0];
+            tmp2=this.colors[5][1];
+            this.colors[5][0]=this.colors[1][3];
+            this.colors[5][1]=this.colors[1][2];
+            this.colors[1][3]=tmp1;
+            this.colors[1][2]=tmp2;
+
+            this.colors[1][3]=this.colors[2][3];
+            this.colors[2][2]=this.colors[2][2];
+            this.colors[2][3]=tmp1;
+            this.colors[2][2]=tmp2;
+
+            this.colors[2][3]=this.colors[3][3];
+            this.colors[2][2]=this.colors[3][2];
+            this.colors[3][3]=tmp1;
+            this.colors[3][2]=tmp2;
+        }
+        if (side == 6){
+            tmp1=this.colors[3][1];
+            tmp2=this.colors[3][3];
+            this.colors[4][2]=this.colors[0][0];
+            this.colors[4][3]=this.colors[0][1];
+            this.colors[0][0]=tmp1;
+            this.colors[0][1]=tmp2;
+
+            this.colors[0][0]=this.colors[1][2];
+            this.colors[0][1]=this.colors[1][0];
+            this.colors[1][2]=tmp1;
+            this.colors[1][0]=tmp2;
+
+            this.colors[1][2]=this.colors[4][3];
+            this.colors[1][0]=this.colors[4][2];
+            this.colors[4][3]=tmp1;
+            this.colors[4][2]=tmp2;
+        }
+    }
+
 
 }
